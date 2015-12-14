e6bca417587286c7e864bee350eafafb69f0a74d
Win8 <Win8@10.7.7.100>
2015-12-14 12:31:36 -0500

    oshe  tu te quiero :3

---
+++ JavaApplication46/src/javaapplication46/JavaApplication46.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication46;
+
+import javax.sound.midi.SysexMessage;
+
+/**
+ *
+ * @author Win8
+ */
+public class JavaApplication46 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Oshe tu te quiero :3");
+    }
+    
+    
+    
+}
